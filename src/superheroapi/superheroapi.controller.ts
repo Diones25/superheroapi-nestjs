959085274dd05e02a0b45cf0c2efdbeafe0c1ac6
Diones <pereiradiones987@gmail.com>
2025-03-01 23:53:00 -0300

    criando service e controller para consumir api externa

--- src/superheroapi/superheroapi.controller.ts
+++ src/superheroapi/superheroapi.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { SuperheroapiService } from './superheroapi.service';
 
 @Controller('superheroapi')
-export class SuperheroapiController {}
+export class SuperheroapiController {
+
+  constructor(private readonly superheroapiService: SuperheroapiService) { }
+
+  @Get(':name')
+  async getSearchSuperheroName(@Param('name') name: string) {
+    return this.superheroapiService.getSearchSuperheroName(name);
+  }
+}
--- src/superheroapi/superheroapi.service.ts
+++ src/superheroapi/superheroapi.service.ts
@@ -1,8 +1,24 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable()
 export class SuperheroapiService {
 
-  constructor(private readonly httpService: HttpService) {}
+  constructor(private readonly httpService: HttpService) { }
+
+  private URL: string = "https://www.superheroapi.com/api.php/b830637c398a7092e8870850bf56075d";
+
+  //https://www.superheroapi.com/api.php/b830637c398a7092e8870850bf56075d/search/cyclops
+
+  async getSearchSuperheroName(name: string) {
+    const url = this.URL;
+    try {
+      const response = await firstValueFrom(this.httpService.get(`${url}/search/${name}`));
+      return response.data;
+    } catch (error) {
+      console.error(`Erro ao buscar o herói: ${error}`);
+      throw new Error("Falha ao consumir API externa");
+    }
+  }
 }
